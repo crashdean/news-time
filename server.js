df2dfba096ee371bd173fa312ef18d29abd1623b
raerae1205 <crashdean@hotmail.com>
2019-07-08 18:33:52 -0700

    Added HEROKU to the APP

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsTime", { useNewUrlParser: true });
-
+// mongoose.connect("mongodb://localhost/newsTime", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 // Routes
 
 // A GET route for scraping the echoJS website
